2d23ba8c1f03d1adbd83541df1e5551cf9c3ff9f
Hari, Inukollu <hari.inukollu@ttu.edu>
2018-05-14 15:04:26 -0500

    Updated multiple nodes.

--- multiplenodes/node1/user-custom-exports.sh
+++ multiplenodes/node1/user-custom-exports.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export DOMAIN="" 
-export USERNAME="" 
-export PASSWORD="" 
+export DOMAIN="isreehari.node1.io"
+export USERNAME="isreehari"
+export PASSWORD="SreeharI@123#"
 export SCRIPT_REPO=""
-export IP="" 
-export NODE_IP="" 
-export DISK="" 
+export IP="192.168.130.129"
+export NODE_IP="192.168.130.132"
+export DISK="/dev/sdb"
